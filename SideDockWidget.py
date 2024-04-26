505fcab43d8eceb727df17c61f6e507095a16c1c
lauric <lauric.gehu@free.fr>
2024-04-26 17:11:22 +0200

    Ajout des boutons

--- SideDockWidget.py
+++ SideDockWidget.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import QSize, Qt, QPoint
-from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QStyle
+from PyQt6.QtWidgets import QApplication, QWidget, QStyle, QPushButton, QGridLayout, QMainWindow
 from PyQt6.QtGui import *
 
 class Side:
@@ -13,15 +13,54 @@ class SideDockWidget(QMainWindow):
         
     def __init__(self):
         super().__init__()
-        
+       
         self.volets = [None] * 5
+        self.buttons = [None] * 4
+
         self.setFixedSize(400, 600)
-    
+
+        btn = self.buttons[Side.LEFT] = QPushButton('<')
+        btn.setParent(self)
+        btn.move(self.rect().center() - btn.rect().center())
+        btn.show()
+
+        self.buttons[Side.TOP] = QPushButton('^')
+        self.buttons[Side.RIGHT] = QPushButton('>')
+        self.buttons[Side.BOT] = QPushButton('\\/')
+
+        for button in self.buttons:
+            button.hide()
+            button.clicked.connect(self.button_event)
+            button.setFixedSize(25, 25)
+            button.setParent(self)
+
+        self.buttons[Side.TOP].move(int(self.width() / 2 - self.buttons[Side.TOP].width() / 2), 0)
+        self.buttons[Side.RIGHT].move(self.width() - self.buttons[Side.RIGHT].width(), int(self.height() / 2 - self.buttons[Side.TOP].height() / 2))
+        self.buttons[Side.LEFT].move(0, int(self.height() / 2 - self.buttons[Side.TOP].height() / 2))
+        self.buttons[Side.BOT].move(int(self.width() / 2 - self.buttons[Side.TOP].width() / 2), self.height() - self.buttons[Side.BOT].height())
+
+        layout = QGridLayout()
+        #self.setLayout(layout)
+
+    def button_event(self):
+        sender = self.sender()
+        id = self.buttons.index(sender)
+        volet = self.volets[id]
+
+        if volet:
+            isHidden = volet.isHidden()
+            volet.setHidden(not isHidden)
+
     def setDock(self, widget, side : Side):
+        
         self.volets[side] = widget
-        widget.setParent(self)
     
-        if side is not Side.CENTER:
+        if side is Side.CENTER:
+            widget.setParent(self)
+            widget.move(self.rect().center() - widget.rect().center())
+        else:
+            self.buttons[side].show()
+            widget.setParent(self)
             widget.setWindowFlag(Qt.WindowType.Window | Qt.WindowType.FramelessWindowHint)
 
         widget.show()
@@ -30,15 +69,15 @@ class SideDockWidget(QMainWindow):
         return self.volets[side]
 
     def moveEvent(self, a0):
-        height = QApplication.style().pixelMetric(QStyle.PixelMetric.PM_TitleBarHeight)
 
         if self.volets[Side.LEFT]:
             pos_y = int(self.pos().y() + self.height() / 2 - self.volets[Side.LEFT].height() / 2)
             self.volets[Side.LEFT].move(QPoint(self.pos().x() - self.volets[Side.LEFT].size().width(), pos_y))
 
         if self.volets[Side.TOP]:
+            title_bar_height = QApplication.style().pixelMetric(QStyle.PixelMetric.PM_TitleBarHeight)
             pos_x = int(self.pos().x() + self.width() / 2 - self.volets[Side.TOP].width() / 2)
-            self.volets[Side.TOP].move(QPoint(pos_x, a0.pos().y() - height - self.volets[Side.TOP].size().height()))
+            self.volets[Side.TOP].move(QPoint(pos_x, a0.pos().y() - title_bar_height - self.volets[Side.TOP].size().height()))
 
         if self.volets[Side.RIGHT]:
             pos_y = int(self.pos().y() + self.height() / 2 - self.volets[Side.RIGHT].height() / 2)
@@ -58,24 +97,25 @@ if __name__ == "__main__":
 
     left = QWidget()
     left.setWindowTitle("Émotes")
-    left.setMaximumSize(QSize(200, 300))
-    
+    left.setFixedSize(QSize(200, 300))
+
     up = QWidget()
     up.setWindowTitle("Crédits")
-    up.setMaximumSize(QSize(500, 150))
+    up.setFixedSize(QSize(500, 150))
 
     right = QWidget()
     right.setWindowTitle("Données")
-    right.setMaximumSize(QSize(200, 300))
+    right.setFixedSize(QSize(200, 300))
 
     bottom = QWidget()
     bottom.setWindowTitle("Maze solver")
-    bottom.setMaximumSize(QSize(500, 150))
+    bottom.setFixedSize(QSize(500, 150))
 
     center = QLabel()
     center.setText("hello")
-    center.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
-    center.setScaledContents(True)
+    center.setFixedSize(300, 500)
+    center.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    center.setStyleSheet("background-color: yellow;")
     
     docker.setDock(left, Side.LEFT)
     docker.setDock(up, Side.TOP)
