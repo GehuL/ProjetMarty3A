bca0ba51ed5df061f1726f12183f4060336499b3
Jaurel Fosset <Jaurel_Fosset@etu.u-bourgogne.fr>
2024-05-15 12:09:16 +0200

    ajoute une position pour les boutons

--- mainscreen.py
+++ mainscreen.py
@@ -8,18 +8,22 @@ class MainScreen(QWidget):
 
         self.button_left = QPushButton("⬅️", parent=self)
         self.button_left.clicked.connect(self.left_clicked)
+        self.button_left.move(50, 100)
         self.button_left.show()
 
         self.button_right = QPushButton("➡️", parent=self)
-        self.button_right.clicked.connect(self.left_clicked)
+        self.button_right.clicked.connect(self.right_clicked)
+        self.button_left.move(100, 100)
         self.button_right.show()
 
         self.button_up = QPushButton("⬆️", parent=self)
-        self.button_up.clicked.connect(self.left_clicked)
+        self.button_up.clicked.connect(self.up_clicked)
+        self.button_left.move(50, 50)
         self.button_up.show()
 
         self.button_down = QPushButton("⬇️", parent=self)
-        self.button_down.clicked.connect(self.left_clicked)
+        self.button_down.clicked.connect(self.down_clicked)
+        self.button_left.move(50, 150)
         self.button_down.show()
 
     def left_clicked(self):
