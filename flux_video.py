7b6a2e907c63c508fb611f25bce79f7d51b42694
LouisDecouzon <louis.decouzon.pro@gmail.com>
2024-05-17 15:17:06 +0200

    fonction
    le code pour afficher la camera n'etait pas dans une fonction

--- flux_video.py
+++ flux_video.py
@@ -1,18 +1,18 @@
 import cv2
 import numpy as np
-#from martypy import Marty
-#my_marty = Marty("wifi","192.168.0.53")
-#my_marty.dance()
-
 #besoin d'etre connecte a marty 
-cap=cv2.VideoCapture(0)#capture la webcam du pc, a voir comment on capture celle de marty
-while True:
-    ret,frame=cap.read()
-    cv2.imshow("frame",frame)#possiblite de resize ici 
-    if cv2.waitKey(1)==ord("q"):#exit when q is pressed 
-        break
-cap.release()
-cv2.destroyAllWindows()
+
+def video_capture():#parametres? Marty?
+    cap=cv2.VideoCapture(0)#capture la webcam du pc, a voir comment on capture celle de marty
+    while True:
+        ret,frame=cap.read()
+        cv2.imshow("frame",frame)#possiblite de resize ici 
+        if cv2.waitKey(1)==ord("q"):#exit when q is pressed 
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    
+video_capture()
 
 
 
