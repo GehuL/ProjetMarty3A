3708354e36627d9b8adf30ebc1093e2435eded43
LouisDecouzon <louis.decouzon.pro@gmail.com>
2024-05-17 17:28:27 +0200

    video dans widget
    la video ne s'affiche plus dans une fenetre a part mais dans un Qwidget.
    il reste a integrer ce code dans le fichier mainscreen.py pour car la video sera affichee sur le widget central

--- flux_video.py
+++ flux_video.py
@@ -1,18 +1,57 @@
+import sys 
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
 import cv2
-import numpy as np
-#besoin d'etre connecte a marty 
 
-def video_capture():#parametres? Marty?
-    cap=cv2.VideoCapture(0)#capture la webcam du pc, a voir comment on capture celle de marty
-    while True:
-        ret,frame=cap.read()
-        cv2.imshow("frame",frame)#possiblite de resize ici 
-        if cv2.waitKey(1)==ord("q"):#exit when q is pressed 
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+class MainWindow(QWidget):
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        
+        self.VBL=QVBoxLayout()
+        
+        self.FeedLabel=QLabel()
+        self.VBL.addWidget(self.FeedLabel)
+        
+        self.cancel_button=QPushButton("Cancel")
+        self.cancel_button.clicked.connect(self.CancelFeed)
+        self.VBL.addWidget(self.cancel_button)
+        
+        self.worker1= Worker1()
+        self.worker1.ImageUpdate.connect(self.ImageUpdateSlot)
+        self.worker1.start()
+        
+        self.setLayout(self.VBL)
     
-video_capture()
+    def ImageUpdateSlot(self,Image):
+        self.FeedLabel.setPixmap(QPixmap.fromImage(Image))
+        
+        
+    def CancelFeed(self):
+        self.worker1.stop()
+        
+class Worker1(QThread):
+    ImageUpdate=pyqtSignal(QImage)
+    def run(self):
+        self.ThreadActive=True
+        Capture=cv2.VideoCapture(0)
+        while self.ThreadActive:
+            ret,frame=Capture.read()
+            if ret:
+                Image=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                Flipped_Image=cv2.flip(Image,1)
+                Convert_To_Qtformat=QImage(Flipped_Image.data, Flipped_Image.shape[1],Flipped_Image.shape[0],QImage.Format.Format_RGB888)
+                pic = Convert_To_Qtformat.scaled(300,300)
+                self.ImageUpdate.emit(pic)
+        
+    
+    def stop(self):
+        self.ThreadActive=False
+        self.quit()
 
 
-
+if __name__ == "__main__":
+    App=QApplication(sys.argv)
+    Root= MainWindow()
+    Root.show()
+    App.exec()  
