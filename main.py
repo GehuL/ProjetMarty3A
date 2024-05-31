027bb092de370dd86a039967c29a4a9d464e2a62
Lauric Gehu <lg501106@ad.u-bourgogne.fr>
2024-05-31 17:34:05 +0200

    Ajout de switchUI

--- main.py
+++ main.py
@@ -3,22 +3,44 @@ import sys
 from martyconnect import MartyHandler
 from PyQt6.QtWidgets import QApplication
 from SideDockWidget import SideDockWidget, Side
+from switchUI import SwitchUI
 
 from emotes import EmoteScreen
 from datas import DataScreen
 from mainscreen import MainScreen
 from connexion import ConnexionWidget
 
+def onSwitch():
+    global mainWindow, automodeWindow
+    if mainWindow.isVisible():
+        mainWindow.close()
+        automodeWindow = buildAutoModeWindow()
+        automodeWindow.show()
+    elif automodeWindow.isVisible():
+        automodeWindow.close()
+        mainWindow = buildMainWindow()
+        mainWindow.show()
+
+def buildMainWindow():
+    win = SideDockWidget()
+    win.setDock(EmoteScreen(), Side.LEFT)
+    win.setDock(DataScreen(), Side.RIGHT)
+    win.setDock(MainScreen(), Side.CENTER)
+    win.setDock(ConnexionWidget(), Side.BOT)
+    win.setDock(SwitchUI(onSwitch), Side.TOP)
+    return win
+
+def buildAutoModeWindow():
+    win = SideDockWidget()
+    win.setDock(SwitchUI(onSwitch), Side.TOP)
+    return win
+
 app = QApplication.instance()
 if not app:
     app = QApplication(sys.argv)
 
-mainWindow = SideDockWidget()
-
-mainWindow.setDock(EmoteScreen(), Side.LEFT)
-mainWindow.setDock(DataScreen(), Side.RIGHT)
-mainWindow.setDock(MainScreen(), Side.CENTER)
-mainWindow.setDock(ConnexionWidget(), Side.BOT)
+mainWindow = buildMainWindow()
+automodeWindow = buildAutoModeWindow()
 
 mainWindow.show()
 
