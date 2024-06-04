c9ad71f17398b808abd717684fdf8ebbc889886c
Jaurel Fosset <Jaurel_Fosset@etu.u-bourgogne.fr>
2024-06-04 15:05:03 +0200

    modifie l'api pour faire la parcours

--- martyCommunication.py
+++ martyCommunication.py
@@ -1,43 +1,43 @@
-class Info:
-    """On stockera les informations que les Marty doivent se passer entre ici"""
-    def __init__(self) -> None:
-        self.foo = "bar"
+gridSize = (3, 3)
 
 class Marty1:
     def __init__(self) -> None:
+        self.isFinishined = False
+        self.isFinishedAnalyzed = False
         pass
 
-    def start() -> Info:
-        """Commence à résoudre le labyrinthe (c'est toujours Marty1 qui commence), renvoie une instance de Info à transmettre à Marty2"""
-        return Info()
+    def analyzeTile(self, index):
+        """Analyze one of the tile, makes Marty turn when he is on the edge of the grid"""
+        return "color"
+
+    def isFinishedAnalyzed(self):
+        return self.isFinishedAnalyzed
+
+
+    def step(self, course, index):
+        """Makes the step associated with course[index]"""
+        print("Making a step :|, n°" + index)
     
-    def step(info: Info) -> Info:
-        """Fait une autre étape de la résolution du labyrinthe à l'aide des infos transmises par Marty2, renvoie une instance de Info à transmettre à Marty2"""
-        return Info()
-    
-    def isFinished() -> bool:
+    def isFinished(self) -> bool:
         """Renvoie true si on à résolue le labyrinthe (plus besoin d'appeler step), false sinon"""
-        return False
+        return self.isFinishined
     
 class Marty2:
     def __init__(self) -> None:
+        self.isFinishined = false
         pass
+
+    def analyzeTile(self, index):
+        """Analyze one of the tile, makes Marty turn when he is on the edge of the grid"""
+        return "color"
+
+    def step(self, course, index):
+        """Makes the step associated with course[index]"""
+        print("Making a step :|, n°" + index)
     
-    def step(info: Info) -> Info:
-        """Fait une autre étape de la résolution du labyrinthe à l'aide des infos transmises par Marty1, renvoie une instance de Info à transmettre à Marty1"""
-        return Info()
-    
-    def isFinished() -> bool:
+    def isFinished(self) -> bool:
         """Renvoie true si on à résolue le labyrinthe (plus besoin d'appeler step), false sinon"""
-        return False
-    
+        return self.isFinishined
 
 def solveMaze():
-    marty1 = Marty1()
-    marty2 = Marty2()
-
-    info = marty1.start()
-
-    while not marty1.isFinished() or not marty2.isFinished():
-        info = marty2.step(info)
-        info = marty1.step(info)
+    pass
